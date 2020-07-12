abf65a226af7cc4192e82d5b6fa45ae52cbaa277
wangx <91907@163.com>
2020-07-12 17:54:38 +0800

    first commit

---
+++ gulimall-member/src/test/java/com/wxx/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxx.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
