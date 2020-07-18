5a0ac5741c5b237e1ca037be2af7253f69f5fb19
wangx <91907@163.com>
2020-07-18 22:55:36 +0800

    阿里oss文件上传

---
+++ gulimall-third-party/src/test/java/com/wxx/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,44 @@
+package com.wxx.gulimall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+//        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
+//// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+//        String accessKeyId = "LTAI4GKa8FtVxdSp2Kqk4gZt";
+//        String accessKeySecret = "po1HBZqtcb1j5KxE7dCsUDztdIBvPZ";
+//
+//// 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+// 上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\Wang\\Desktop\\docs\\pics\\oppo.png");
+        ossClient.putObject("wangxin3-gulimall", "oppo.png", inputStream);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传成功");
+    }
+
+}
