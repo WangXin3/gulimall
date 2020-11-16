8ee4af908b0812deb911f016cf1383a832cc1642
wangxin <91907@163.com>
2020-11-16 21:10:04 +0800

    feat: 认证中心

---
+++ gulimall-auth-server/src/main/java/com/wxx/gulimall/auth/GulimallAuthServerApplication.java
@@ -0,0 +1,22 @@
+package com.wxx.gulimall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author wangxin
+ * @date 2020-11-16
+ * 认证中心
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallAuthServerApplication.class, args);
+	}
+
+}
---
+++ gulimall-auth-server/src/main/java/com/wxx/gulimall/auth/config/MyWebConfig.java
@@ -0,0 +1,23 @@
+package com.wxx.gulimall.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author wangxin
+ * @date 2020/11/16
+ */
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+
+    /**
+     * 视图映射
+     * @param registry /
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/reg.html").setViewName("reg");
+    }
+}
